50cc907d4ccf51b44c2eb0a7f3372971ed9a2fb5
Stjerndal <stjernd@kth.se>
2014-12-28 00:45:06 +0100

    Adding tabs.

--- Flatlanders/app.js
+++ Flatlanders/app.js
@@ -5,18 +5,40 @@
 		this.products = gems;
 	});
 
+	app.controller("PanelController", function() {
+		this.tab  = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab; 
+		};
+	});
+
 	var gems = [
 		{
-			name: 'Dodecahedron',
-			price: 2,
+			name: 'Dodecahedron Gem',
+			price: 2.95,
 			description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-			canPurchase: true,
+			images: [
+				{
+					full: 'img/1.png',
+					thumb: 'img/thumb.png'
+				}
+			]
 		},
 		{
 			name: 'Pentagonal Gem',
 			price: 5.95,
 			description: 'Five is the number of edgerinos surrounding this beatuty.',
-			canPurchase: false,
+			images: [
+				{
+					full: 'img/2.png',
+					thumb: '/thumb.png'
+				},
+			],
 		},
 
 	];
